671878f6a8a13e101b165f30f565d234bbc4e02d
msm-lz <msmlearningzone@gmail.com>
2014-05-02 17:43:38 +0100

    end of learningzone 02may

--- app/models/Showing.java
+++ app/models/Showing.java
@@ -2,55 +2,48 @@ package models;
 
 import java.util.List;
 import java.util.Map;
-import play.modules.mongodb.jackson.MongoDB;
-import net.vz.mongodb.jackson.JacksonDBCollection;
-import net.vz.mongodb.jackson.Id;
-import net.vz.mongodb.jackson.ObjectId;
+
+
+import com.mongodb.*;
 
 
 /**
  * Created by rory.payne on 02/05/14.
  */
+
 public class Showing {
 
-    @Id
-    @ObjectId
     public String id;
 
     public String movie_title;
     public String[] director;
     public String[] cast;
     public String release_date;
-    public String running_length;
+    public double running_length;
     public String age_rating;
     public String age_rating_img_url;
     public String synopsis;
     public String img_url;
-    public Map<String, String> prices;
-    public Map<String, String> time_and_date;
-    public Map<String, String> sales_data;
+    public Map<String, Double> prices;
+    public Map<String, Object[]> time_and_date;
 
-    public static JacksonDBCollection<Showing, String> collection = MongoDB.getCollection("showings",
-                                                                                                  Showing.class,
-                                                                                                  String.class
-                                                                                                  );
 
     public Showing() {
 
     }
 
+
     public Showing(String movie_title,
                    String[] director,
                    String[] cast,
                    String release_date,
-                   String running_length,
+                   double running_length,
                    String age_rating,
                    String age_rating_img_url,
                    String synopsis,
                    String img_url,
-                   Map<String, String> prices,
-                   Map<String, String> time_and_date,
-                   Map<String, String> sales_data) {
+                   Map<String, Double> prices,
+                   Map<String, Object[]> time_and_date) {
         this.movie_title=movie_title;
         this.director=director;
         this.cast=cast;
@@ -62,10 +55,35 @@ public class Showing {
         this.img_url=img_url;
         this.prices=prices;
         this.time_and_date=time_and_date;
-        this.sales_data=sales_data;
     }
 
-    public List<String> getListPageData() {
-        return Showing.collection.findOne().toString().toCharArray();
+//    public static List<Showing> all() {
+//        return Showing.collection.find().toArray();
+//    }
+
+    public static String listView() {
+        try {
+            MongoClient mongoClient = new MongoClient("localhost", 27017);
+
+            DB db = mongoClient.getDB("cinema");
+
+            DBCollection coll= db.getCollection("showings");
+
+            BasicDBObject keys = new BasicDBObject();
+            keys.put("movie_title",1);
+            keys.put("running_length",1);
+            keys.put("age_rating",1);
+            keys.put("img_url",1);
+            keys.put("synopsis",1);
+            keys.put("_id", 0);
+            DBCursor all = coll.find(null, keys);
+
+            return all.toArray().toString();
+
+        }
+        catch (Exception e) {
+            return "oops" + e;
+        }
     }
+
 }
